c530da4b1a8d087da3a0c13eb6e5a00c498eb4bc
Aleksey Shagraev <alex-shagraev@ya.ru>
2020-09-17 16:44:52 +0300

    added remote training and applying run modes

--- main.go
+++ main.go
@@ -5,15 +5,19 @@ providing API for solving simple regression tasks
 package main
 
 import (
+	"bufio"
 	"context"
 	"flag"
+	"fmt"
 	"log"
 	"net/http"
 	"os"
+	"strconv"
 )
 
-func main() {
-	var port = flag.String("port", "80", "http service port")
+func runHandler() {
+	flag.Bool("server", true, "run the solving server")
+	port := flag.String("port", "80", "run http handler using this port")
 	flag.Parse()
 
 	ctx := context.Background()
@@ -29,5 +33,57 @@ func main() {
 	err = http.ListenAndServe(":" + *port, nil)
 	if err != nil {
 		log.Fatal("ListenAndServe:", err)
+	}
+}
+
+func runTrain() {
+	client := NewTrainingClient()
+
+	instances, err := loadInstancesFromTSV(os.Stdin)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	result, err := client.requestTraining(instances)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println(result)
+}
+
+func runApply() {
+	client := NewCalculatingClient()
+
+	scanner := bufio.NewScanner(os.Stdin)
+	for scanner.Scan() {
+		text := scanner.Text()
+		arg, err := strconv.ParseFloat(text, 64)
+		if err != nil {
+			log.Fatalf("invalid float: %v", text)
+		}
+
+		result, err := client.requestCalculation(arg)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		fmt.Println(result)
+	}
+}
+
+func main() {
+	if len(os.Args) < 2 {
+		log.Fatal("choose mode: server, train or apply")
+	}
+
+	if os.Args[1] == "--server" {
+		runHandler()
+	}
+	if os.Args[1] == "--train" {
+		runTrain()
+	}
+	if os.Args[1] == "--apply" {
+		runApply()
 	}
 }
